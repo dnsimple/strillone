a2a2c3811054a0a8a118c4ca36d55b1ad4d10cb3
Anthony Eden <me@anthonyeden.com>
2017-02-09 11:51:21 +0100

    Implement deliver-once by checking request IDs against a cache (#1)

--- server_test.go
+++ server_test.go
@@ -48,3 +48,31 @@ func TestSlack(t *testing.T) {
 		t.Errorf("POST /slack expected response\n\t%v\ngot\n\t%v", want, got)
 	}
 }
+
+func TestSlackTwice(t *testing.T) {
+	payload := `{"data": {"domain": {"id": 1, "name": "example.com", "state": "hosted", "token": "domain-token", "account_id": 1010, "auto_renew": false, "created_at": "2016-02-07T14:46:29.142Z", "expires_on": null, "updated_at": "2016-02-07T14:46:29.142Z", "unicode_name": "example.com", "private_whois": false, "registrant_id": null}}, "actor": {"id": "1", "entity": "user", "pretty": "example@example.com"}, "account": {"id": 1010, "display": "User", "identifier": "user"}, "name": "domain.create", "api_version": "v2", "request_identifier": "096bfc29-2bf0-40c6-0000-f03b1f8521f1"}`
+
+	request, _ := http.NewRequest("POST", "/slack/-/-/-", strings.NewReader(payload))
+	response := httptest.NewRecorder()
+
+	server.Slack(response, request, httprouter.Params{})
+
+	if want := http.StatusOK; want != response.Code {
+		t.Errorf("POST /slack expected HTTP %v, got %v", want, response.Code)
+	}
+	if want, got := "", response.Header().Get(headerProcessingStatus); want != got {
+		t.Errorf("POST /slack X-Processing-Status expected empty, got %v", got)
+	}
+
+	requestDuplicate, _ := http.NewRequest("POST", "/slack/-/-/-", strings.NewReader(payload))
+	responseDuplicate := httptest.NewRecorder()
+
+	server.Slack(responseDuplicate, requestDuplicate, httprouter.Params{})
+
+	if want := http.StatusOK; want != responseDuplicate.Code {
+		t.Errorf("POST /slack (duplicate) expected HTTP %v, got %v", want, responseDuplicate.Code)
+	}
+	if want, got := "skipped;already-processed", responseDuplicate.Header().Get(headerProcessingStatus); want != got {
+		t.Errorf("POST /slack (duplicate) X-Processing-Status expected %v, got %v", want, got)
+	}
+}
