fb7adf2e761de3e4828ce74ca462e13d4a07b7d8
Javier Acero <j4cegu@gmail.com>
2020-05-25 18:08:16 +0200

    Introduce test for default message
    The goal here is being able to write specs for the message formatters. I
    have used the simplest one as it requires less data about the event.

--- message_test.go
+++ message_test.go
@@ -1,8 +1,38 @@
 package strillone
 
 import (
+  "fmt"
 	"testing"
+
+	"github.com/dnsimple/dnsimple-go/dnsimple/webhook"
 )
+
+type TestMessagingService struct {
+	Name string
+}
+
+func NewTestMessagingService(name string) *TestMessagingService {
+	return &TestMessagingService{Name: name}
+}
+func (*TestMessagingService) FormatLink(url, name string) string {
+	return fmt.Sprintf("<%s|%s>", url, name)
+}
+func (*TestMessagingService) PostEvent(event *webhook.Event) (string, error) {
+	return "ok", nil
+}
+
+func Test_Message_DefaultMessage(t *testing.T) {
+	service := NewTestMessagingService("dummyMessagingService")
+	account := webhook.Account{Identifier: "ID", Display: "john.doe@gmail.com"}
+	actor := webhook.Actor{Pretty: "john.doe@email.com"}
+	event := webhook.Event{Actor: &actor, Account: &account, Name: "event.name"}
+
+	result := Message(service, &event)
+
+	if want, got := "[<john.doe@gmail.com|https://dnsimple.com/a/0/account>] john.doe@email.com performed event.name", result; want != got {
+		t.Fatalf("Expected %v, got %v", want, got)
+	}
+}
 
 func Test_fmtURL(t *testing.T) {
 	if want, got := "https://dnsimple.com/a/1010/domains/1", fmtURL("/a/%v/domains/%v", "1010", 1); want != got {
