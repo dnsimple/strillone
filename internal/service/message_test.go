ca91c5a3849ed5245bc6092fed5c65c1359f9e38
Simone Carletti <5387+weppos@users.noreply.github.com>
2025-09-11 12:07:38 +0200

    Sort test alphabetically

--- internal/service/message_test.go
+++ internal/service/message_test.go
@@ -90,6 +90,28 @@ func Test_Message_AccountEventData(t *testing.T) {
 	})
 }
 
+func Test_Message_CertificateEventData(t *testing.T) {
+	t.Run("certificate.issue", func(t *testing.T) {
+		service := NewTestMessagingService("dummyMessagingService")
+		payload := `{"data": {"certificate": {"id": 12345, "domain_id": 67890, "common_name": "example.com", "state": "issued", "auto_renew": false, "created_at": "2023-09-20T14:30:19Z", "updated_at": "2023-09-20T14:30:19Z"}}, "name": "certificate.issue", "actor": {"id": "2", "entity": "user", "pretty": "simone.carletti@dnsimple.com"}, "account": {"id": 625, "display": "Webhook Tests", "identifier": "webhooks@example.com"}, "api_version": "v2", "request_identifier": "e3e3d10b-b0cd-498c-9fb6-9afdb44fd19a"}`
+		event, err := webhook.ParseEvent([]byte(payload))
+		assert.NoError(t, err)
+
+		result := xservice.Message(service, event)
+		assert.Equal(t, "[<Webhook Tests|https://dnsimple.com/a/625/account>] simone.carletti@dnsimple.com issued the certificate <example.com|https://dnsimple.com/a/625/domains/67890/certificates/12345>", result)
+	})
+
+	t.Run("certificate.remove_private_key", func(t *testing.T) {
+		service := NewTestMessagingService("dummyMessagingService")
+		payload := `{"data": {"certificate": {"id": 12345, "domain_id": 67890, "common_name": "example.com", "state": "issued", "auto_renew": false, "created_at": "2023-09-20T14:30:19Z", "updated_at": "2023-09-20T14:30:19Z"}}, "name": "certificate.remove_private_key", "actor": {"id": "2", "entity": "user", "pretty": "simone.carletti@dnsimple.com"}, "account": {"id": 625, "display": "Webhook Tests", "identifier": "webhooks@example.com"}, "api_version": "v2", "request_identifier": "e3e3d10b-b0cd-498c-9fb6-9afdb44fd19a"}`
+		event, err := webhook.ParseEvent([]byte(payload))
+		assert.NoError(t, err)
+
+		result := xservice.Message(service, event)
+		assert.Equal(t, "[<Webhook Tests|https://dnsimple.com/a/625/account>] simone.carletti@dnsimple.com deleted the private key for the certificate <example.com|https://dnsimple.com/a/625/domains/67890/certificates/12345>", result)
+	})
+}
+
 func Test_Message_DomainEventData(t *testing.T) {
 	t.Run("domain.transfer_lock_disable", func(t *testing.T) {
 		service := NewTestMessagingService("dummyMessagingService")
@@ -151,28 +173,6 @@ func Test_Message_DNSSECEventData(t *testing.T) {
 
 		result := xservice.Message(service, event)
 		assert.Equal(t, "[<Webhook Tests|https://dnsimple.com/a/625/account>] support@dnsimple.com completed DNSSEC key rotation for the zone <example-20230920163010.com|https://dnsimple.com/a/625/domains/example-20230920163010.com>", result)
-	})
-}
-
-func Test_Message_CertificateEventData(t *testing.T) {
-	t.Run("certificate.issue", func(t *testing.T) {
-		service := NewTestMessagingService("dummyMessagingService")
-		payload := `{"data": {"certificate": {"id": 12345, "domain_id": 67890, "common_name": "example.com", "state": "issued", "auto_renew": false, "created_at": "2023-09-20T14:30:19Z", "updated_at": "2023-09-20T14:30:19Z"}}, "name": "certificate.issue", "actor": {"id": "2", "entity": "user", "pretty": "simone.carletti@dnsimple.com"}, "account": {"id": 625, "display": "Webhook Tests", "identifier": "webhooks@example.com"}, "api_version": "v2", "request_identifier": "e3e3d10b-b0cd-498c-9fb6-9afdb44fd19a"}`
-		event, err := webhook.ParseEvent([]byte(payload))
-		assert.NoError(t, err)
-
-		result := xservice.Message(service, event)
-		assert.Equal(t, "[<Webhook Tests|https://dnsimple.com/a/625/account>] simone.carletti@dnsimple.com issued the certificate <example.com|https://dnsimple.com/a/625/domains/67890/certificates/12345>", result)
-	})
-
-	t.Run("certificate.remove_private_key", func(t *testing.T) {
-		service := NewTestMessagingService("dummyMessagingService")
-		payload := `{"data": {"certificate": {"id": 12345, "domain_id": 67890, "common_name": "example.com", "state": "issued", "auto_renew": false, "created_at": "2023-09-20T14:30:19Z", "updated_at": "2023-09-20T14:30:19Z"}}, "name": "certificate.remove_private_key", "actor": {"id": "2", "entity": "user", "pretty": "simone.carletti@dnsimple.com"}, "account": {"id": 625, "display": "Webhook Tests", "identifier": "webhooks@example.com"}, "api_version": "v2", "request_identifier": "e3e3d10b-b0cd-498c-9fb6-9afdb44fd19a"}`
-		event, err := webhook.ParseEvent([]byte(payload))
-		assert.NoError(t, err)
-
-		result := xservice.Message(service, event)
-		assert.Equal(t, "[<Webhook Tests|https://dnsimple.com/a/625/account>] simone.carletti@dnsimple.com deleted the private key for the certificate <example.com|https://dnsimple.com/a/625/domains/67890/certificates/12345>", result)
 	})
 }
 
